468dc2dc044a10ffe71378441fc7b4b4c1354ce7
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-11-08 13:25:58 +0900

    컴포넌트 최적화 - useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -35,9 +35,23 @@ function App() {
 		);
 	};
 
+	const getDiaryAnalysis = useMemo(() => {
+		const goodCount = data.filter(element => element.emotion >= 3).length;
+		const badCount = data.length - goodCount;
+		const goodRatio = (goodCount / data.length) * 100;
+
+		return { goodCount, badCount, goodRatio };
+	}, [data.length]);
+
+	const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
 	return (
 		<div className='App'>
 			<DiaryEditor onCreate={onCreate} />
+			<div>전체 일기 : {data.length}</div>
+			<div>기분 좋은 일기 개수 : {goodCount}</div>
+			<div>기분 나쁜 일기 개수 : {badCount}</div>
+			<div>기분 좋은 일기 비율 : {goodRatio}</div>
 			<DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
 		</div>
 	);
